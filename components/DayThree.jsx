ad599ae65372b93ad29f5fdfdafafeb525bb8875
adriansth <soto.herrera.adrian@gmail.com>
2022-10-22 15:59:18 -0500

    feat: Finished Agenda section.

--- components/DayThree.jsx
+++ components/DayThree.jsx
@@ -1,7 +1,94 @@
 export default function DayThree() {
     return(
         <div>
-            Day Three
+            {/* Main text */}
+            <div className='my-32'>
+                <h2 className='text-[35px] font-header text-darkBrown font-bold my-8'>Lorem ipsum dolor sit amet.</h2>
+                <p className='text-[20px] font-body text-grayBrown mb-8'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa a quia deserunt similique error eveniet, quos possimus illo repellendus, harum quae. Ex optio, 
+                    iste temporibus, quibusdam architecto repellat blanditiis asperiores numquam ut assumenda officia accusamus illo nemo cupiditate eligendi! Ullam minima, ipsum libero 
+                    saepe necessitatibus incidunt voluptatibus voluptates hic facilis!
+                </p>
+                <p className='text-[20px] font-body text-grayBrown mb-8'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga quibusdam sit ut necessitatibus assumenda minima totam pariatur quos magnam laborum, eligendi magni, 
+                    in doloribus explicabo incidunt corporis reiciendis blanditiis consequatur eum? Temporibus veniam accusamus repudiandae quis, totam molestias autem sunt minus aspernatur, 
+                    corporis quod, sed aliquam? Sint, exercitationem? Voluptatibus sunt distinctio odit quo quisquam cumque culpa nihil repudiandae, eveniet saepe.
+                </p>
+                <p className='text-[20px] font-body text-grayBrown'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque temporibus ipsa reprehenderit facilis rerum dolor odit, consectetur saepe culpa amet autem nobis quia repellat 
+                    repellendus esse at possimus harum iusto?
+                </p>
+            </div>
+            {/* Step 1 */}
+            <div className='flex justify-center gap-20'>
+                {/* Figures */}
+                <div className='relative'>
+                    <span className='font-bold text[30px] text-white bg-salmon py-2 px-4 rounded-full absolute z-10'>1</span>
+                    <div className='bg-lightSalmon h-full w-1 opacity-50 ml-[18px] absolute z-0'></div>
+                </div>
+                {/* Text */}
+                <div>
+                    <h3 className='text-darkBrown font-header text-[30px] font-bold'>Lorem ipsum dolor sit amet.</h3>
+                    <p className='text-grayBrown font-body text-[20px] py-16'>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora obcaecati ut laboriosam veniam culpa, accusamus, ad delectus illo laudantium animi inventore, 
+                        amet deserunt? Distinctio rem architecto vel! Quo, earum provident?
+                    </p>
+                </div>
+            </div>
+            {/* Step 2 */}
+            <div>
+                <div className='flex justify-center gap-20'>
+                    {/* Figures */}
+                    <div className='relative'>
+                        <span className='font-bold text[30px] text-white bg-salmon py-2 px-[15px] rounded-full absolute z-10'>2</span>
+                        <div className='bg-lightSalmon h-full w-1 opacity-50 ml-[18px] absolute z-0'></div>
+                    </div>
+                    {/* Text */}
+                    <div>
+                        <h3 className='text-darkBrown font-header text-[30px] font-bold'>Lorem ipsum dolor sit amet.</h3>
+                        <p className='text-grayBrown font-body text-[20px] py-16'>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi ex dicta illo qui dignissimos nihil, mollitia, temporibus aspernatur similique rerum fugiat! Modi 
+                            assumenda deserunt natus inventore mollitia, ratione distinctio aspernatur!
+                        </p>
+                    </div>
+                </div>
+            </div>
+            {/* Step 3 */}
+            <div>
+                <div className='flex justify-center gap-20'>
+                    {/* Figures */}
+                    <div className='relative'>
+                        <span className='font-bold text[30px] text-white bg-salmon py-2 px-[15px] rounded-full absolute z-10'>3</span>
+                        <div className='bg-lightSalmon h-full w-1 opacity-50 ml-[18px] absolute z-0'></div>
+                    </div>
+                    {/* Text */}
+                    <div>
+                        <h3 className='text-darkBrown font-header text-[30px] font-bold'>Lorem ipsum dolor sit amet.</h3>
+                        <p className='text-grayBrown font-body text-[20px] py-16'>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime unde velit officia odit reiciendis hic qui, molestiae totam quasi nostrum culpa mollitia cum soluta quas 
+                            ea sapiente in temporibus! Quibusdam!
+                        </p>
+                    </div>
+                </div>
+            </div>
+            {/* Step 4 */}
+            <div>
+                <div className='flex justify-center gap-20'>
+                    {/* Figures */}
+                    <div className='relative'>
+                        <span className='font-bold text[30px] text-white bg-salmon py-2 px-[15px] rounded-full absolute z-10'>4</span>
+                        <div className='bg-lightSalmon h-full w-1 opacity-50 ml-[18px] absolute z-0'></div>
+                    </div>
+                    {/* Text */}
+                    <div>
+                        <h3 className='text-darkBrown font-header text-[30px] font-bold'>Lorem ipsum dolor sit amet.</h3>
+                        <p className='text-grayBrown font-body text-[20px] py-16'>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime unde velit officia odit reiciendis hic qui, molestiae totam quasi nostrum culpa mollitia cum soluta quas 
+                            ea sapiente in temporibus! Quibusdam!
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
--- components/DayTwo.jsx
+++ components/DayTwo.jsx
@@ -1,7 +1,72 @@
 export default function DayTwo() {
     return(
         <div>
-            Day Two
+            {/* Main text */}
+            <div className='my-32'>
+                <h2 className='text-[35px] font-header text-darkBrown font-bold my-8'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, accusamus?</h2>
+                <p className='text-[20px] font-body text-grayBrown mb-8'>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint architecto earum cum doloremque, ex porro fuga excepturi modi deleniti, possimus, consequuntur 
+                    dolor perspiciatis cupiditate quos necessitatibus non facilis perferendis error? Amet veniam voluptatum laborum, obcaecati rem ullam consequuntur et provident alias 
+                    voluptate maiores maxime adipisci! Iste ipsam eaque quam. Eligendi. 
+                </p>
+                <p className='text-[20px] font-body text-grayBrown'>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga beatae vel doloribus officiis sapiente, magni culpa, asperiores laborum odit omnis quia. Ex velit facilis 
+                    itaque libero inventore, eos suscipit, sed voluptate odit dolor pariatur rerum explicabo! Veritatis obcaecati delectus minus, similique ducimus mollitia est porro voluptate. 
+                    Fugiat ratione necessitatibus quibusdam?
+                </p>
+            </div>
+            {/* Step 1 */}
+            <div className='flex justify-center gap-20'>
+                {/* Figures */}
+                <div className='relative'>
+                    <span className='font-bold text[30px] text-white bg-salmon py-2 px-4 rounded-full absolute z-10'>1</span>
+                    <div className='bg-lightSalmon h-full w-1 opacity-50 ml-[18px] absolute z-0'></div>
+                </div>
+                {/* Text */}
+                <div>
+                    <h3 className='text-darkBrown font-header text-[30px] font-bold'>Lorem ipsum dolor sit amet.</h3>
+                    <p className='text-grayBrown font-body text-[20px] py-16'>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora obcaecati ut laboriosam veniam culpa, accusamus, ad delectus illo laudantium animi inventore, 
+                        amet deserunt? Distinctio rem architecto vel! Quo, earum provident?
+                    </p>
+                </div>
+            </div>
+            {/* Step 2 */}
+            <div>
+                <div className='flex justify-center gap-20'>
+                    {/* Figures */}
+                    <div className='relative'>
+                        <span className='font-bold text[30px] text-white bg-salmon py-2 px-[15px] rounded-full absolute z-10'>2</span>
+                        <div className='bg-lightSalmon h-full w-1 opacity-50 ml-[18px] absolute z-0'></div>
+                    </div>
+                    {/* Text */}
+                    <div>
+                        <h3 className='text-darkBrown font-header text-[30px] font-bold'>Lorem ipsum dolor sit amet.</h3>
+                        <p className='text-grayBrown font-body text-[20px] py-16'>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi ex dicta illo qui dignissimos nihil, mollitia, temporibus aspernatur similique rerum fugiat! Modi 
+                            assumenda deserunt natus inventore mollitia, ratione distinctio aspernatur!
+                        </p>
+                    </div>
+                </div>
+            </div>
+            {/* Step 3 */}
+            <div>
+                <div className='flex justify-center gap-20'>
+                    {/* Figures */}
+                    <div className='relative'>
+                        <span className='font-bold text[30px] text-white bg-salmon py-2 px-[15px] rounded-full absolute z-10'>3</span>
+                        <div className='bg-lightSalmon h-full w-1 opacity-50 ml-[18px] absolute z-0'></div>
+                    </div>
+                    {/* Text */}
+                    <div>
+                        <h3 className='text-darkBrown font-header text-[30px] font-bold'>Lorem ipsum dolor sit amet.</h3>
+                        <p className='text-grayBrown font-body text-[20px] py-16'>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime unde velit officia odit reiciendis hic qui, molestiae totam quasi nostrum culpa mollitia cum soluta quas 
+                            ea sapiente in temporibus! Quibusdam!
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
